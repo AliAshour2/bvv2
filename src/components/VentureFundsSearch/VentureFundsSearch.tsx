d5ce8b29a22c1f61c4748a33f3446c4d6b67daee
AliAshour2 <aliashour468@gmail.com>
2024-08-07 13:54:52 +0300

    change the table

--- src/components/VentureFundsSearch/VentureFundsSearch.tsx
+++ src/components/VentureFundsSearch/VentureFundsSearch.tsx
@@ -1,3 +1,5 @@
+
+
 import { useState } from "react";
 import { funds } from "../../constants";
 
@@ -10,7 +12,7 @@ const VentureFundsSearch = () => {
   const [selectedInvestmentSize, setSelectedInvestmentSize] = useState("");
   const [selectedRegion, setSelectedRegion] = useState("");
 
-  const handlePageChange = (page : number) => {
+  const handlePageChange = (page: number) => {
     if (page > 0 && page <= totalPages) {
       setCurrentPage(page);
       if (page === pageRange[2] && page < totalPages) {
@@ -38,7 +40,7 @@ const VentureFundsSearch = () => {
   );
 
   return (
-    <div id="Venture-Fundes-Search" className="flex flex-col container mx-auto py-8">
+    <div id="Venture-Funds-Search" className="flex flex-col container mx-auto py-8">
       <div className="-m-1.5 overflow-x-auto">
         <div className="p-1.5 min-w-full inline-block align-middle">
           <div className="border rounded-lg divide-y divide-gray-200">
@@ -81,13 +83,14 @@ const VentureFundsSearch = () => {
                     "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 ps-4 pe-9 flex gap-x-2 text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-neutral-600",
                     "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 dark:bg-neutral-900 dark:border-neutral-700",
                     "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:text-neutral-200 dark:focus:bg-neutral-800",
-                    "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-3.5 text-blue-600 dark:text-blue-500 \" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>",
+                    "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-3.5 text-blue-600 dark:text-blue-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>",
                     "extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"><svg class=\"shrink-0 size-3.5 text-gray-500 dark:text-neutral-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m7 15 5 5 5-5\"/><path d=\"m7 9 5-5 5 5\"/></svg></div>"
-                  }' className="hidden"
+                  }'
+                className="py-2 px-3   border-gray-200 shadow-sm rounded-lg text-sm"
                 value={selectedInvestmentSize}
                 onChange={(e) => setSelectedInvestmentSize(e.target.value)}
               >
-                <option value="Investment Size">Investment Size</option>
+                <option disabled value="Investment Size">Investment Size</option>
                 <option>$0-$100K</option>
                 <option>$100K-$500K</option>
                 <option>$500K-$1M</option>
@@ -107,20 +110,21 @@ const VentureFundsSearch = () => {
                     "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 ps-4 pe-9 flex gap-x-2 text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-neutral-600",
                     "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 dark:bg-neutral-900 dark:border-neutral-700",
                     "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:text-neutral-200 dark:focus:bg-neutral-800",
-                    "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-3.5 text-blue-600 dark:text-blue-500 \" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>",
+                    "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-3.5 text-blue-600 dark:text-blue-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>",
                     "extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"><svg class=\"shrink-0 size-3.5 text-gray-500 dark:text-neutral-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m7 15 5 5 5-5\"/><path d=\"m7 9 5-5 5 5\"/></svg></div>"
-                  }' className="hidden"
+                  }'
+                className="py-2 px-3  border-gray-200 shadow-sm rounded-lg text-sm"
                 value={selectedRegion}
                 onChange={(e) => setSelectedRegion(e.target.value)}
               >
-                <option value="Region">Region</option>
-                <option value={"USA"} >North America</option>
+                <option disabled value="Region">Region</option>
+                <option value="USA">North America</option>
                 <option>East Asia & Pacific</option>
                 <option>Africa Sub-Saharan</option>
-                <option value={"Europe"}>Europe & Central Asia</option>
-                <option value={"MENA"}>Middle East & North Africa</option>
+                <option value="Europe">Europe & Central Asia</option>
+                <option value="MENA">Middle East & North Africa</option>
                 <option>Latin America & Caribbean</option>
-                <option value={"Southeast Asia"}>South East Asia</option>
+                <option value="Southeast Asia">South East Asia</option>
               </select>
               {/* End Select */}
             </div>
@@ -201,6 +205,7 @@ const VentureFundsSearch = () => {
                         <a
                           href={fund.WEBSITE}
                           target="_blank"
+                          rel="noopener noreferrer"
                           className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none"
                         >
                           Visit
@@ -226,18 +231,19 @@ const VentureFundsSearch = () => {
                   <span aria-hidden="true">«</span>
                   <span className="sr-only">Previous</span>
                 </button>
-                {pageRange.map((page, index) => (
+                {pageRange.map((page) => (
                   <button
-                    key={index}
-                    type="button"
-                    onClick={() => handlePageChange(page)}
-                    className={`min-w-[40px] p-2.5 inline-flex justify-center items-center gap-x-2 rounded-full text-sm font-medium ${
-                      currentPage === page
-                        ? "bg-blue-600 text-white"
-                        : "text-gray-800 hover:bg-gray-100 focus:bg-gray-100"
-                    }`}
-                  >
-                    {page}
+                  key={page}
+                  type="button"
+                  onClick={() => handlePageChange(page)}
+                  className={`inline-flex items-center justify-center w-9 h-9 rounded-full border ${
+                    page === currentPage
+                      ? "border-blue-600 bg-blue-50 text-blue-600"
+                      : "border-gray-100 bg-white text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:text-blue-600"
+                  } disabled:opacity-50 disabled:pointer-events-none`}
+                >
+                  {page}
+
                   </button>
                 ))}
                 <button
@@ -260,3 +266,6 @@ const VentureFundsSearch = () => {
 };
 
 export default VentureFundsSearch;
+
+
+
